6c0906012da8b740bd7d3352e54e297a03950a6e
IHaveNoIdeaWhatImDo-ing <84041839+IHaveNoIdeaWhatImDo-ing@users.noreply.github.com>
2023-02-22 17:09:36 +0200

    Create zad2.cpp

---
+++ Pract1/zad2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N = 5, amount = 0;
+    do {
+        cout << "Enter the amount of whole numbers: ";
+        cin >> N;
+    } while (N <= 0);
+    
+    char letter;
+    cout << "Enter anascii character: ";
+    cin >> letter;
+    
+    cout << "Enter the whole numbers: ";
+    int* arr = new int[N];
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> arr[i];
+        if (arr[i] > letter) amount++;
+    }
+    
+    cout << '\n' << "The amount of numbers larger than the letter's ascii code is " << amount;
+    
+    delete[] arr;
+
+    return 0;
+}
